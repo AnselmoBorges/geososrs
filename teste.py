ee6d7209fa25b823c55ba31fdd632206844826d8
Anselmo Borges <33070146+AnselmoBorges@users.noreply.github.com>
2023-03-20 22:31:15 +0000

    teste

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
-print('oi')
+def buscaCEP(cep):
+    import urllib.request
+    import json
+    try:
+        url = "http://viacep.com.br/ws/%s/json/" % cep
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        dados = json.loads(response.read().decode('utf-8'))
+        return dados
+    except:
+        return False
+
+    buscaCEP('01521000')

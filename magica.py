270d26e7563de39660f1531ec39dad74912ce184
anselmoborges <anselmoborges>
2023-03-21 00:25:57 +0000

    atualizado

--- magica.py
+++ magica.py
@@ -1,2 +1,8 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ## Essse belo notebook faz uma magica
+# MAGIC A de te tornar um grande vagabundo, rs
 
+# COMMAND ----------
+
+## Crie uma função Python que busque o CEP
